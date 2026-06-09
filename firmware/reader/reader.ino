a525d5cffe8d982870ac41a2abaa15379f72bcab
djStolen <djordjestojicevic@gmail.com>
2026-06-09 22:04:50 +0000

    Added Heartbeat LED in reader

--- firmware/reader/reader.ino
+++ firmware/reader/reader.ino
@@ -9,7 +9,14 @@
 TM1637Display display(CLK, DIO);
 MotronicDecoder decoder(2500, 150);
 
+// Heartbeat variables
+const int HEARTBEAT_LED = 13;        // Onboard LED on Arduino Nano
+unsigned long lastHeartbeatTime = 0; 
+const long heartbeatInterval = 500;  // Blink every 500ms
+bool ledState = LOW;
+
 void setup() {
+  pinMode(HEARTBEAT_LED, OUTPUT);
   pinMode(SIGNAL_PIN, INPUT);
   pinMode(RESET_BTN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(SIGNAL_PIN), handleInterrupt, FALLING);
@@ -29,6 +36,14 @@ void localReset() {
 }
 
 void loop() {
+  // --- NON-BLOCKING HEARTBEAT ---
+  unsigned long currentMillis = millis();
+  if (currentMillis - lastHeartbeatTime >= heartbeatInterval) {
+    lastHeartbeatTime = currentMillis;
+    ledState = !ledState; // Toggle the LED state
+    digitalWrite(HEARTBEAT_LED, ledState);
+  }
+  // ------------------------------
   // Poll time delta progressions 
   if (decoder.checkTimeout(millis())) {
     display.showNumberDec(decoder.getLatestCode(), true);
